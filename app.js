edcf4e83727a8c169f056f99bb2e26db792541f2
tinhntctu <nguyentrungtinhtvt@gmail.com>
2024-03-06 23:26:00 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-const contactsRouter = require("./app/routes/contact.route");
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-    res.json({message:"Welcome to contact book application."});
-});
+
 app.use("/api/contacts", contactsRouter);
+// handle 404 response
+app.use( async (req, res, next) => {
+  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  return next(new ApiError(404, "NotFound"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // Middleware xử lý lỗi tập trung.
+  // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json({ messages: "Wellcome to contact book application" });
+});
+
 module.exports = app;
