ef3e21efc9976f894343af5b894d328e94eb5302
tinhntctu <nguyentrungtinhtvt@gmail.com>
2024-03-06 23:02:26 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app.js
@@ -0,0 +1,12 @@
+const contactsRouter = require("./app/routes/contact.route");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.json({message:"Welcome to contact book application."});
+});
+app.use("/api/contacts", contactsRouter);
+module.exports = app;
