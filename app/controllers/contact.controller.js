56bc3b9d3f883bd151bfa992db0cd3ae0ce07dec
tinhntctu <tinhck22v7k542@vlvh.ctu.edu.vn>
2024-03-09 03:35:33 +0700

    lab2

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -45,17 +45,3 @@ exports.create = async (req, res, next) =>{
         );
     }
 };
-exports.create = async (req, res, next) =>{
-    if(!req.boby?.name){
-        return next(new ApiError(400, "Name can not be empty"));
-    }
-    try {
-        const contactService = new ContactService(MongoDB.client);
-        const document = await contactService.create(req.boby);
-        return res.send(document);
-    } catch (error) {
-        return next(
-            new ApiError(500, "An error occurred while creating the contact")
-        );
-    }
-};
